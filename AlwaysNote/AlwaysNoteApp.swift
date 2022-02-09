9afc1c287dbd0c96edfac50da7bce45dad343cc7
Ryan <ryan.wennekes@hotmail.com>
2022-02-09 15:49:20 +0100

    Initial Commit

---
+++ AlwaysNote/AlwaysNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysNoteApp.swift
+//  AlwaysNote
+//
+//  Created by Ryan Wennekes on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
